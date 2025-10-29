ea5f786881b5178865ba52fd1f1c398286ccf188
LukaszKusmierz <lukasz@lukspeed.eu>
2025-10-29 21:12:14 +0100

    currency at orders page and shipping methods quick fixed

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -114,7 +114,7 @@ const OrdersPage = () => {
                     </span>
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
-                                    <div className="text-sm text-gray-900">{order.total} PLN</div>
+                                    <div className="text-sm text-gray-900">{order.total} EUR</div>
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">
                                     <div className="text-sm text-gray-900">{order.itemCount}</div>
